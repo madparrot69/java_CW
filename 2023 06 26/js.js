494e8dcda253e54a549fbd673d9e419396ed2bf1
K. L <121451594+madparrot69@users.noreply.github.com>
2023-08-30 20:45:18 +0300

    2023 06 26

---
+++ 2023 06 26/js.js
@@ -0,0 +1,31 @@
+// // let str = false;
+
+// // let a = 5;
+
+// // let b = 6;
+
+// // console.log(a + b);
+
+// // console.log(a - b);
+
+// // console.log(a * b);
+
+// // console.log(a / b);
+
+// // console.log(a % b);
+
+// let number = 10;
+
+// let num2 = 2;
+
+// // number = number + num2;
+
+// number += num2;
+
+// console.log(number);
+
+
+let number = +prompt('Press is number');
+
+console.log(typeof number);
+console.log(number);
