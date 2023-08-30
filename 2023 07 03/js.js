885a6b684a18fec9b09cec9dda304cc3bc463636
K. L <121451594+madparrot69@users.noreply.github.com>
2023-08-30 20:45:44 +0300

    2023 07 03

---
+++ 2023 07 03/js.js
@@ -0,0 +1,37 @@
+// // // let a = `5`;
+
+// // // let b = Number(a);
+
+// // // // console.log(typeof Number(a));
+// // // console.log(typeof b);
+// //  let width = `50.50px`;
+
+// //  console.log(Number.parseFloat(width));
+
+// // let a = Number(`51`);
+
+// // console.log(Number.isNaN(a));
+
+// // let b = Number(`akjsdkj`);
+
+// // console.log(Number.isFinite(b));
+
+// // let result;
+
+// // result = 5 + 5 + `5`;
+
+// // console.log(result);
+// // console.log(typeof result);
+
+// let name = `Khrystyna`;
+
+// // console.log(name.length - 1);
+
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+
+let message = `Найкраща помста — величезний успіх`;
+
+console.log(message.indexOf(`помста`));
+
+console.log(message.includes(`jkf`));
