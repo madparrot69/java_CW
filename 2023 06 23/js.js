06f3dcb817b8bfe06f45b69df2334c984ba48507
K. L <121451594+madparrot69@users.noreply.github.com>
2023-08-30 20:44:28 +0300

    2023 06 23

---
+++ 2023 06 23/js.js
@@ -0,0 +1,29 @@
+var moneyBank = 25;
+
+console.log(moneyBank);
+
+mobeyBank = 40;
+
+console.log(typeof moneyBank);
+
+let name = "khrystyna lokidottir";
+
+console.log(name.toUpperCase());
+
+console.log(name.length);
+
+console.log(typeof name);
+
+const BACKGROUNG_COLOR = 'black';
+
+
+//alert('hello world');
+
+// let prm = prompt('are you boy?');
+
+// console.log(prm);
+
+
+let conf = confirm(' i`m khrystyna');
+
+console.log(typeof conf);
