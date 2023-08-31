d71c8b9fe25ae2f2db306014c3e3ec3a2259b26c
K. L <121451594+madparrot69@users.noreply.github.com>
2023-08-31 10:02:42 +0300

    2023 07 07

---
+++ 2023 07 07/js.js
@@ -0,0 +1,74 @@
+
+
+// `use strict`;
+// let message =  
+//     `Найкраща`;
+
+// console.log(message.startsWith(`Най`));
+// console.log(message.endsWith(`пі`))
+
+// console.log(message.padStart(15, `?`));
+// console.log(message.padEnd(10, `1`));
+
+// console.log(message.length);
+
+
+
+// let age = 15;
+
+// let name = `Khrystyna`;
+
+// let surname = `Lokidottir`;
+
+
+// let hi = `hi, my name is ` + name + ` ` + surname + `. i am ` + age + ` years old.`;
+
+// console.log(hi);
+
+// let easy = `hi, my name is ${name} ${surname}. i am ${age} years old.`
+
+// console.log(easy);
+
+// let a = +prompt(`enter your age`);
+
+// let n = prompt(`enter your name`);
+
+// let s = prompt (`enter your surname`);
+
+// let omg = alert(`hi, my name is ${n} ${s}. i am ${a} years old.`);
+
+//1
+
+let surName = `Khrystyna Lokidottir`;
+
+console.log(surName.length);
+
+//2
+
+let name = `Khrystyna`;
+
+console.log(name.toUpperCase());
+
+//3
+
+let name2 = `Khrystyna`;
+
+console.log(name2.charAt(0));
+
+//4
+
+let surName2 = `Khrystyna Lokidottir`;
+
+console.log(surName2.indexOf(` `));
+
+//5
+
+let name3 = `Khrystyna`;
+
+console.log(name3.replace(`K`, `@`));
+
+//6
+
+let java = `JavaScript`;
+
+console.log(java.substring(0, 4));
---
+++ pe13/java.js
@@ -0,0 +1,18 @@
+// let btn = document.querySelector("button");
+// let div = document.querySelector("#container");
+
+// btn.addEventListener("click", function () {
+//    div.innerHTML = `
+//    <div class= "item">
+//    <h3>Назва</h3>
+//    <p>текст текст текст</p>
+//    </div>
+//    `;
+// });
+
+let a = document.querySelector("a");
+let div = document.querySelector(".play");
+
+a.addEventListener("click", function () {
+   div.style.left = "50px";
+});
